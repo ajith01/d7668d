ae789e6608cf4636b3827ba20bed3246fe3dd0f5
Ajith Kusalakumar <ajith_me@outlook.com>
2022-06-28 22:56:54 -0400

    Fix variables, name and remove let statements. Added more validations

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,6 +3,14 @@ const { Post, UserPost } = require('../db/models');
 
 const router = express.Router();
 
+const sortByOptions = ['id', 'reads', 'likes', 'popularity'];
+const directionOptions = ['asc', 'desc'];
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const FORBIDDEN = 403;
+const NOT_FOUND = 404;
+const OK = 200;
+
 /**
  * Create a new blog post
  * req.body is expected to contain {text: required(string), tags: optional(Array<string>)}
@@ -41,58 +49,54 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-//Part 1: Fetching blog posts
+/*
+ * Get a list of posts
+ * req.query is expected to contain { authorID: required(String), sortBy optional(string), direction optional(String)}
+ */
 router.get('/', async (req, res, next) => {
   try {
-    //-----------Validations -----------//
     //Validate user
     if (!req.user) {
-      return res.send(401).json({ message: 'Log in required' });
+      return res.send(UNAUTHORIZED).json({ message: 'Log in required' });
     }
 
     // check if authorIds is sent
     if (!req.query.authorIds) {
-      return res.send(400).json({ error: 'Must provide authorIds' });
+      return res.send(BAD_REQUEST).json({ error: 'Must provide authorIds' });
     }
 
-    //make single element into array
-    let authors = req.query.authorIds;
-
     //authors is passed as a string, convert to array
-    //if not an array assume its a single element
-
-    authors = authors.split(',').map(Number);
+    const authors = req.query.authorIds.split(',').map(Number);
     //if an array, make sure all elements are positive numbers
     if (!validateArrays(authors)) {
       return res
-        .send(400)
+        .send(BAD_REQUEST)
         .json({ error: 'authorIds must be a positive number' });
     }
 
-    const sortVal = req.query.sortBy ? req.query.sortBy : 'id';
-    const asdOrDsn = req.query.direction ? req.query.direction : 'asc';
+    const sortParameter = req.query.sortBy ? req.query.sortBy : 'id';
+    const sortDirection = req.query.direction ? req.query.direction : 'asc';
 
-    if (!['id', 'reads', 'likes', 'popularity'].includes(sortVal)) {
+    if (!sortByOptions.includes(sortParameter)) {
       return res
-        .send(400)
+        .send(BAD_REQUEST)
         .json({ error: 'sortBy must be one of id, reads, likes, popularity' });
     }
 
-    if (!['asc', 'desc'].includes(asdOrDsn)) {
+    if (!directionOptions.includes(sortDirection)) {
       return res
-        .send(400)
+        .send(BAD_REQUEST)
         .json({ error: 'direction must be one of asc, desc' });
     }
 
-    //----------- End of Validations -----------//
+    const existingPosts = new Set();
+    const posts = [];
 
-    let existingPosts = new Set();
-    let posts = [];
     for (let i = 0; i < authors.length; i++) {
       // fetch posts from each author
-      let authorPosts = await Post.getPostsByUserId(authors[i]);
+      const authorPosts = await Post.getPostsByUserId(authors[i]);
       authorPosts.forEach((post) => {
-        //check if post is already in the list
+        //check if post is already in the list if not add to list
         if (!existingPosts.has(post.id)) {
           post.tags = post.tags.split(',');
           posts.push(post.dataValues);
@@ -102,35 +106,44 @@ router.get('/', async (req, res, next) => {
     }
 
     //sort objects
-    if (asdOrDsn === 'asc') {
+    if (sortDirection === 'asc') {
       posts.sort((a, b) => {
-        return a[sortVal] - b[sortVal];
+        return a[sortParameter] - b[sortParameter];
       });
     } else {
       posts.sort((a, b) => {
-        return b[sortVal] - a[sortVal];
+        return b[sortParameter] - a[sortParameter];
       });
     }
 
-    return res.status(200).json({ posts: posts });
+    return res.status(OK).json({ posts: posts });
   } catch (error) {
     next(error);
   }
 });
 
-//Part 2: Updating a blog post
-
+/*
+ * update a post by id
+ * req.body is expected to contain {authorIds:optional(Array<Integer>) text: optional(string), tags: optional(Array<string>)}
+ */
 router.patch('/:postId', async (req, res, next) => {
   try {
-    //-----------Validations -----------//
-
     //Validate user
     if (!req.user) {
-      return res.send(401).json({ message: 'Log in required' });
+      return res.send(UNAUTHORIZED).json({ message: 'Log in required' });
     }
 
     if (isInvalidNumber(req.params.postId)) {
-      return res.send(400).json({ error: 'postId must be a positive number' });
+      return res
+        .send(BAD_REQUEST)
+        .json({ error: 'postId must be a positive number' });
+    }
+
+    //get the  post to be updated
+    const FoundPost = await Post.findOne({ where: { Id: req.params.postId } });
+
+    if (!FoundPost) {
+      return res.send(NOT_FOUND).json({ error: 'Post not found' });
     }
 
     //validate if user is author of post
@@ -142,36 +155,31 @@ router.patch('/:postId', async (req, res, next) => {
     });
 
     if (!postByUser) {
-      return res.status(401).json({
-        message:
-          'You are not authorized to edit this post or the post does not exist',
+      return res.status(FORBIDDEN).json({
+        message: 'You are not authorized to edit this post',
       });
     }
 
-    //get the  post to be updated
-    const FoundPost = await Post.findOne({ where: { Id: req.params.postId } });
-    //the post should exist as it was validated before
-
     //update the post
     const { text, tags } = req.body;
-    let authorIds = req.body.authorIds;
-    console.log(authorIds);
+    const authorIds = req.body.authorIds;
+
     if (authorIds) {
       //update authorIds in UserPost
 
       if (!Array.isArray(authorIds)) {
         //if not an array assume its a single element
-        return res.send(400).json({ error: 'authorIds must be an array' });
+        return res
+          .send(BAD_REQUEST)
+          .json({ error: 'authorIds must be an array' });
       }
 
       if (!validateArrays(authorIds)) {
         return res
-          .send(400)
+          .send(BAD_REQUEST)
           .json({ error: 'authorIds must be a positive number' });
       }
 
-      //should we keep the old records? should the author be able to delete his own id?
-      //if we delete the old records, the author will not be able to edit the post anymore
       //assumes we delete all the old records for the post and create new ones with the updated authorIds
       await UserPost.destroy({ where: { postId: req.params.postId } });
 
@@ -184,9 +192,12 @@ router.patch('/:postId', async (req, res, next) => {
         await createdRecord.save();
       });
     }
+
     if (text) {
-      if (text.length === 0) {
-        return res.send(400).json({ error: 'text cannot be empty' });
+      if (typeof text !== 'string' || text.length === 0) {
+        return res
+          .send(BAD_REQUEST)
+          .json({ error: 'text must be a non-empty string' });
       }
       //update text in Post
       FoundPost.text = text;
@@ -194,7 +205,7 @@ router.patch('/:postId', async (req, res, next) => {
 
     if (tags) {
       if (tags.length === 0) {
-        return res.send(400).json({ error: 'tags must not be empty' });
+        return res.send(BAD_REQUEST).json({ error: 'tags must not be empty' });
       }
       //assumes that tags are replaced with new ones
       FoundPost.tags = tags.join(',');
@@ -205,7 +216,7 @@ router.patch('/:postId', async (req, res, next) => {
       await FoundPost.save();
     }
 
-    let post = FoundPost.get({ plain: true });
+    const post = FoundPost.get({ plain: true });
     const foundIds = await UserPost.findAll({
       attributes: ['userId'],
       where: { postId: req.params.postId },
@@ -218,7 +229,7 @@ router.patch('/:postId', async (req, res, next) => {
 
     post.authorIds = authors;
     post.tags = post.tags.split(',');
-    return res.status(200).json({ post });
+    return res.status(OK).json({ post });
   } catch (error) {
     next(error);
   }
@@ -229,11 +240,11 @@ function isInvalidNumber(number) {
 }
 
 function validateArrays(arrays) {
-  for (let i = 0; i < arrays.length; i++) {
-    if (isInvalidNumber(arrays[i])) {
+  arrays.forEach((element) => {
+    if (isInvalidNumber(element)) {
       return false;
     }
-  }
+  });
   return true;
 }
 
